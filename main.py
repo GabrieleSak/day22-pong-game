bf47c5b2bf0732e5e4fb38a80b74b7a91a2f706e
GabrieleSak <gabriele.sakalauskiene@yahoo.com>
2022-10-14 11:45:33 +0300

    Add Paddle class, initialize left and right paddles and their controls with "up", "down" and "w", "s" keys

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
 from turtle import Turtle, Screen
+from paddle import Paddle
 
 screen = Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
+screen.title("Pong")
+screen.tracer(0)
+
+
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+
+screen.listen()
+screen.onkeypress(r_paddle.up, "Up")
+screen.onkeypress(r_paddle.down, "Down")
+
+screen.onkeypress(l_paddle.up, "w")
+screen.onkeypress(l_paddle.down, "s")
+
+
+game_is_on = True
+while game_is_on:
+    screen.update()
 
 screen.exitonclick()
---
+++ paddle.py
@@ -0,0 +1,22 @@
+from turtle import Turtle
+
+
+class Paddle(Turtle):
+    def __init__(self, coordinates):
+        super().__init__()
+        self.coordinates = coordinates
+        self.shape("square")
+        self.color("white")
+        self.shapesize(stretch_wid=1, stretch_len=5)
+        self.penup()
+        self.goto(coordinates)
+        self.speed("fastest")
+        self.setheading(90)
+
+    def up(self):
+        self.setheading(90)
+        self.forward(20)
+
+    def down(self):
+        self.setheading(270)
+        self.forward(20)
