fda614035337059569f349bf1840a549083fc336
GabrieleSak <gabriele.sakalauskiene@yahoo.com>
2022-10-14 13:13:24 +0300

    Add Ball class, initialize ball and make it move

--- ball.py
+++ ball.py
@@ -10,11 +10,6 @@ class Ball(Turtle):
         self.goto(0, 0)
 
     def move(self):
-        # angle = self.towards(goal)
-        # self.setheading(angle)
-        # self.forward(100)
-        # self.speed("slow")
-        # self.goto(goal)
         new_x = self.xcor() + 10
         new_y = self.ycor() + 10
         self.goto(new_x, new_y)
